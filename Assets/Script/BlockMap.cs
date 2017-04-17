1c4ddcae3f34ee180ecbd168c76d51a921a414f8
JJ <6107120@gmail.com>
2017-04-17 17:55:39 +0900

    lightly finish blockmap

--- Assets/Script/BlockMap.cs
+++ Assets/Script/BlockMap.cs
@@ -36,11 +36,13 @@ public class BlockMap : MonoBehaviour {
 
 		//set block on first floor
 		allFloors.Add(baseBlockAccessible());
+		allFloors.Add(blockAccessible(allFloors,1));
+		allFloors.Add(blockAccessible(allFloors,2));
 
 		for (int y=0; y<mapSize.y; y++){
 			for (int x=0; x<mapSize.x; x++){
 				for (int z=0; z<mapSize.z; z++){
-					if(allFloors[0][x,z]){
+					if(allFloors[y][x,z]){
 						Vector3 blockPosition = CoordToPosition(x, y, z);
 						Transform newBlock = Instantiate(blockPrefab, blockPosition + Vector3.up * 0.5f, Quaternion.identity);
 						newBlock.parent = mapHolder;
@@ -153,63 +155,55 @@ public class BlockMap : MonoBehaviour {
 				for(int z=-1; z<=1; z++){		
 					int neighbourX = block.x + x;
 					int neighbourZ = block.z + z;
-					seedCount ++;
 					if(Mathf.Abs(x) != Mathf.Abs(z)){
 						if(neighbourX >= 0 && neighbourX < eachFloorBlocks.GetLength(0) && neighbourZ >= 0 && neighbourZ < eachFloorBlocks.GetLength(1)) {
-							if(!eachFloorBlocks[neighbourX, neighbourZ] && Utility.TrueOrFalse(3,seedCount*seed)) {
+							if(!eachFloorBlocks[neighbourX, neighbourZ] && Utility.TrueOrFalse(2,seedCount*seed)) {
 								eachFloorBlocks[neighbourX, neighbourZ] = true;
 								queue.Enqueue(new Coord(neighbourX, 0 ,neighbourZ));
 								checkCount++;
+								break;
 							}
 						}
 					}
 				}
 			}
-			queue.Enqueue(block);
-			if(checkCount >= blockCount || checkCount >20)
-			break;
+			seedCount ++;
+			if((seedCount < 15 && queue.Count < 2) || Utility.TrueOrFalse(2,seedCount*seed))
+				queue.Enqueue(block);
+			if(checkCount >= blockCount || seedCount >15)
+				break;
 		}
 		
 		return eachFloorBlocks;
 	}
 
 	bool[,] blockAccessible(List<bool[,]> allFloors, int y) {
-		bool[,] thisFloorBlocks = new bool[(int)mapSize.x, (int)mapSize.z];
-		bool[,] beforeFloorBlocks = new bool[(int)mapSize.x, (int)mapSize.z];
-		Queue<Coord> queue = new Queue<Coord> ();
-		queue.Enqueue(startPoint);
-		thisFloorBlocks[startPoint.x, startPoint.z] = true;
-
+		bool[,] beforeFloorBlocks = allFloors[y-1];
+		bool[,] canFloorBlocks = new bool[(int)mapSize.x, (int)mapSize.z];
+		bool[,] couldFloorBlocks = new bool[(int)mapSize.x, (int)mapSize.z];
 
 		int seedCount = 0;
-		int checkCount = 1;
-		// for(int i=0; i<blockCount; i++) {
-		while(true){
-			if(queue.Count == 0)
-				break;
-			Coord block = queue.Dequeue();
-			for(int x=-1; x<=1; x++){
-				for(int z=-1; z<=1; z++){		
-					int neighbourX = block.x + x;
-					int neighbourZ = block.z + z;
-					seedCount ++;
-					if(Mathf.Abs(x) != Mathf.Abs(z)){
-						if(neighbourX >= 0 && neighbourX < thisFloorBlocks.GetLength(0) && neighbourZ >= 0 && neighbourZ < thisFloorBlocks.GetLength(1)) {
-							if(!thisFloorBlocks[neighbourX, neighbourZ] && Utility.TrueOrFalse(3,seedCount*seed)) {
-								thisFloorBlocks[neighbourX, neighbourZ] = true;
-								queue.Enqueue(new Coord(neighbourX, 0 ,neighbourZ));
-								checkCount++;
+
+		for (int xSize=0; xSize<mapSize.x; xSize++){
+			for (int zSize=0; zSize<mapSize.z; zSize++){
+				if(beforeFloorBlocks[xSize,zSize]){
+					for(int x=-1; x<=1; x++){
+						for(int z=-1; z<=1; z++){
+							int neighbourX = xSize + x;
+							int neighbourZ = zSize + z;
+							if(Mathf.Abs(x) != Mathf.Abs(z) || (x==0 && z==0)){
+								if(neighbourX >= 0 && neighbourX < beforeFloorBlocks.GetLength(0) && neighbourZ >= 0 && neighbourZ < beforeFloorBlocks.GetLength(1)) {
+									if(Utility.TrueOrFalse(3,seedCount*seed))
+									canFloorBlocks[neighbourX, neighbourZ] = true;
+								}
 							}
+							seedCount++;
 						}
 					}
 				}
 			}
-			queue.Enqueue(block);
-			if(checkCount >= blockCount || checkCount >20)
-			break;
-		}
-		
-		return thisFloorBlocks;
+		}	
+		return canFloorBlocks;
 	}
 
 	Vector3 CoordToPosition(int x, int y, int z) {
